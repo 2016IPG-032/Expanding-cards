22f0bb6a351db745edf2455d8601561588f337bd
Deepak Kumar Kannaujiya <38581960+2016IPG-032@users.noreply.github.com>
2021-12-13 20:39:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const panels=document.querySelectorAll('.panel')
+
+panels.forEach((panel)=>{
+
+    //console.log(panel);
+
+    panel.addEventListener('click',()=>{
+        removeActiveClasses();
+        panel.classList.add('active');
+    })
+})
+
+function removeActiveClasses(){
+
+    panels.forEach(panel=>{
+        panel.classList.remove('active');
+
+    })
+}
